627f2cae792e5f4520859df3a21f8b04e6c1e0f0
ryan-sam-daniel <edu.ryan.grad@gmail.com>
2023-12-12 23:43:05 +0530

    mergeSort

---
+++ MergeSort.java
@@ -0,0 +1,13 @@
+public class MergeSort {
+    public static void main(String[] args){
+    int arr[]={3,5,1,4,6,2};
+    for(int n : arr){
+        System.out.print(n+" ");
+    }
+    System.out.println();
+    System.out.println("After sorting");
+    for(int n : arr){
+        System.out.print(n + " ");
+    }
+}
+}
